6fb38a592e78ea8dd05b58ce64127104b6eafb62
Loganzheng <zhengzhihao994@outlook.com>
2021-11-15 18:33:28 +0800

    add no.114丑数 II

---
+++ src/0114丑数 II/index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function (n) {
+  const dp = [];
+  dp[1] = 1;
+  let p2 = 1;
+  let p3 = 1;
+  let p5 = 1;
+  for (let i = 2; i <= n; i++) {
+    const num2 = dp[p2] * 2;
+    const num3 = dp[p3] * 3;
+    const num5 = dp[p5] * 5;
+    dp[i] = Math.min(num2, num3, num5);
+    if (dp[i] === num2) {
+      p2++;
+    }
+    if (dp[i] === num3) {
+      p3++;
+    }
+    if (dp[i] === num5) {
+      p5++;
+    }
+  }
+  return dp[n];
+};
